c3b00f84954b1592ee297949ef347fcfd5c2bc1a
Chenz <chenzuo0730@outloo.com>
2016-03-31 10:49:42 +0800

    22

---
+++ CordovaLearn/CordovaLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CordovaLearn
+//
+//  Created by MAC on 16/3/30.
+//  Copyright © 2016年 chenzuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CordovaLearn/CordovaLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CordovaLearn
+//
+//  Created by MAC on 16/3/30.
+//  Copyright © 2016年 chenzuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
