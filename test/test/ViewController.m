1ebbdb7a953ad97ab82ab0212155539e1813e129
Chenz <chenzuo0730@outloo.com>
2016-04-28 16:08:22 +0800

    大大大大更

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "FirstViewController.h"
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *numTextField;
 @end
 
 @implementation ViewController
@@ -21,17 +23,41 @@
 //    NSDecimalNumber * allPriceNum = [[NSDecimalNumber init]initWithString:@"8500"];
 //    NSLog( @"%@",allPriceNum);
     
-    NSString *filePath = [[NSBundle mainBundle]pathForResource:@"BankListXE"ofType:@"json"];
-    //根据文件路径读取数据
-    NSData *jdata = [[NSData alloc]initWithContentsOfFile:filePath];
+//    NSString *filePath = [[NSBundle mainBundle]pathForResource:@"BankListXE"ofType:@"json"];
+//    //根据文件路径读取数据
+//    NSData *jdata = [[NSData alloc]initWithContentsOfFile:filePath];
     
+    CGFloat row = ceil(3/4.0);
+    NSLog(@"%f",row);
     
-    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(iWant) name:@"11123456" object:nil];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)firstBtn:(id)sender {
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"123456" object:nil];
+    FirstViewController * nextViewController = [[FirstViewController alloc]initWithNibName:@"FirstViewController" bundle:nil];
+    
+    [self presentViewController:nextViewController animated:YES completion:^{
+        
+    }];
+}
+- (void)iWant
+{
+    NSLog(@"123456 已经被触发");
+}
+
+
+- (IBAction)Compute:(UIButton *)sender {
+    CGFloat x = (CGFloat)[self.numTextField.text doubleValue];
+    CGFloat row = ceil(x/4.0);
+    [sender setTitle:[NSString stringWithFormat:@"%f",row] forState:UIControlStateNormal];
+}
+
 
 @end
