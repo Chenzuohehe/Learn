8e7fb07b4c0f33a2d74fe541c44d601f01c05d92
Chenz <chenzuo0730@outloo.com>
2016-06-14 15:30:00 +0800

    swift 初试

--- swiftLearn/swiftLearn/ViewController.swift
+++ swiftLearn/swiftLearn/ViewController.swift
@@ -9,10 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = "The width is"
+        let width = 94
+        let widthLabel = label + String(width)
+        print( "宽度 " + widthLabel)
+        
+        let apple = 3
+        let oranges = 5
+        let appleSummary = "I have \(apple) apples."
+        let orangeSummary = "I have \(oranges) oranges"
+        
+        print(appleSummary + "\n" + orangeSummary)
+        
+        
+        var shoppingList = ["catfish","water","tulips","blue paint"]
+        shoppingList[1] = "bottle of water"
+        
+        print(shoppingList)
+        
+        
+        var occupations = [
+        "Malcolm":"Captain",
+        "Kaylee":"Mechanic",
+        ]
+        occupations["Jayne"] = "Public Relations"
+        
+        print(occupations)
+        
+        //()空初始化？
+        let array = [String]()
+        let dictionary = [String:Float]()
+        
+        print(array)
+        print(dictionary)
+        
+        let numberList = [75,43,106,87,12]
+        
+        for score in  numberList{
+            if score > 50{
+                print(String(score) + ">50")
+            }else{
+                print(String(score) + "<＝50")
+            }
+        }
+        
+        
+        var optionalString:String? = "hello"
+        print(optionalString == nil)
+        
+        var optionaName:String? = "John Appleseed"
+        var greeting = "Hello!"
+        if let name = optionaName {
+            greeting = "Hello, \(name)"
+        }
+        print(greeting)
+        
+        let nickName: String? = nil
+        let fullName: String = "John Appleseed"
+        let informalGreeting = "Hi \(nickName ?? fullName)"
+        
+        print(informalGreeting)
+        
+        let vegetable = "red peper"
+        switch vegetable {
+        case "celery":
+            print("is celery")
+        case "cucumber","watercress":
+            print( "is cucumber or watercress")
+        case let x where x.hasSuffix("peper"):
+            print("Is it a spicy \(x)?")
+        default:
+            print("everything tastes good in soup")
+            
+        }
+        
+        var total = 0
+        
+        for i in 0...100 {
+            total += i
+        }
+        print(total)
+        
+        print( greet("bob", day: "tuesday"))
+        
+        let statistics = calculateStatustics([5,3,100,3,9])
+        print(statistics.sum)
+        print(statistics.1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +112,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func greet(name: String, day: String) -> String {
+        return "hello, \(name),today is \(day)"
+    }
+    func calculateStatustics(scores: [Int]) -> (min:Int,max:Int,sum:Int) {
+        var min = scores[0]
+        var max = scores[0]
+        var sum = 0
+        
+        for score in scores {
+            if score > max {
+                max = score
+            }else if score < min{
+                min = score
+            }
+            sum += score
+        }
+        return (min, max ,sum)
+    }
+    
+    
 }
 
---
+++ swiftLearn/test.swift
@@ -0,0 +1,11 @@
+//
+//  test.swift
+//  swiftLearn
+//
+//  Created by MAC on 16/6/14.
+//  Copyright © 2016年 chenzuo. All rights reserved.
+//
+
+import Foundation
+
+
