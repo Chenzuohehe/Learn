785e2a74a79049d9202f5163093b234244c9dec4
陈佐 <chenzuo0730@outlook.com>
2016-11-03 16:42:57 +0800

    base

---
+++ nei/nei/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  nei
+//
+//  Created by ChenZuo on 2016/11/3.
+//  Copyright © 2016年 ChenZuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
